ef0863c1ead03c00250f86bc6a8b961eefd721c8
EdwardWu <bluehome.wu@gmail.com>
2026-04-19 01:28:41 +0800

    chore: bump versionCode 4 -> 5, versionName 1.2.1 -> 1.3.0 (three-layer keep-alive)
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "tw.bluehomewu.devicemonitor"
         minSdk = 30
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
