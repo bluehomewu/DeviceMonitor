96cbf2abc36343d4a1e5154a229219d907371ebc
EdwardWu <bluehome.wu@gmail.com>
2026-04-19 10:13:53 +0800

    chore: bump version to 1.6.2 (versionCode 12)
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "tw.bluehomewu.devicemonitor"
         minSdk = 29
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.6.1"
+        versionCode = 12
+        versionName = "1.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
