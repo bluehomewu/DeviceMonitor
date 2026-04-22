db750cb1cbab837deeb417b80ec9dd27784b1301
EdwardWu <bluehome.wu@gmail.com>
2026-04-22 21:27:38 +0800

    fix: bump versionCode 25->26, versionName 1.10.0->1.10.1
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "tw.bluehomewu.devicemonitor"
         minSdk = 29
         targetSdk = 36
-        versionCode = 25
-        versionName = "1.10.0"
+        versionCode = 26
+        versionName = "1.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
