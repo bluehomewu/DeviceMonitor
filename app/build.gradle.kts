7b5838774aaf83dd29cb661ba0e311e7e1fa3afd
EdwardWu <bluehome.wu@gmail.com>
2026-05-17 00:50:48 +0800

    fix: bump versionCode 33->34, versionName 1.14.0->1.15.0
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "tw.bluehomewu.devicemonitor"
         minSdk = 29
         targetSdk = 36
-        versionCode = 33
-        versionName = "1.14.0"
+        versionCode = 34
+        versionName = "1.15.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
